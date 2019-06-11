8d7c859f1f8daa9eb5027a5d04a2f81ee6d48705
PolCarCat <polcarcat@gmail.com>
2019-06-11 19:32:03 +0200

    1.1 done

---
+++ Wwise Adventure Game No Sound/Assets/Scripts/IntroAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAudio : MonoBehaviour
+{
+    AudioSource source;
+
+    public AudioClip main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+
+            source.clip = main;
+            source.Play();
+            source.loop = true;
+        }
+    }
+}
