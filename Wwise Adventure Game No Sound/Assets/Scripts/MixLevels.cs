1518f565838cac1ee3c37f69e7bf5ee1f95bbc4d
Normanbg <normanbg98@gmail.com>
2019-06-12 17:46:48 +0200

    5 donete

---
+++ Wwise Adventure Game No Sound/Assets/Scripts/MixLevels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    
+    public void SetMasterLvl(float masterLvl)
+    {
+        audioMixer.SetFloat("MasterVol", masterLvl);
+    }
+
+    public void SetMusicLvl(float musicLvl)
+    {
+        audioMixer.SetFloat("MusicVol", musicLvl);
+    }
+}
